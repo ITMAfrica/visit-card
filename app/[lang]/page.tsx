09a7c80d1c3c0c59d79acad3bc2c3681740d1ebb
minganda <mingandajeereq@gmail.com>
2025-06-04 12:14:53 +0100

    feat: save change in meta data

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -4,18 +4,31 @@ import { getUserInKaziPro } from '@/helpers';
 import { Suspense } from 'react';
 export async function generateMetadata(params: any) {
     const { searchParams: { id } } = params
+
     let title = ""
     let description = ""
+    let picture = ""
+
     await getUserInKaziPro(id)
         .then(async (response: any) => {
             title = `${response?.data.firstName} ${response?.data.name}`
             description = `${response?.contract?.job?.name
                 ? response?.contract?.job?.name
                 : response.id == 8778 ? 'Chairman' : ""}`
+            picture = response?.picture ||
+                'https://www.itmafrica.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fcd.38a5bff4.png&w=256&q=75'
         })
     return {
+        icon: picture,
+        images: [{ url: picture }],
         title: 'Carte de visite : ' + title,
-        description
+        description,
+        openGraph: {
+            title: 'Carte de visite : ' + title,
+            description,
+            type: 'article',
+            images: [{ url: picture }],
+        },
     };
 }
 export default async function Home({ params }: any) {
